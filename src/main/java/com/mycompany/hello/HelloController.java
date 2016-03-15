e1529e20ae2b3788a139f1f310a733b953b22939
Chet Kuhn <gochet@gmail.com>
2016-03-14 19:33:49 -0500

    first commit

---
+++ src/main/java/com/mycompany/hello/HelloController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Chet Kuhn is the greatest!";
+    }
+
+}
